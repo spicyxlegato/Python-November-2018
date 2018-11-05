5141f83fdd3cafdeb1cc415e50e4c1b7206dad43
Robert John Castillo <gracenote83@gmail.com>
2018-11-05 07:40:15 -0500

    Example use of urllib and iterating through the text file's words.

--- urllib_example.py
+++ urllib_example.py
@@ -1,16 +1,54 @@
+#!/usr/bin/env python3
+# Newb note: Above comment is useful for user to identify
+# which interpreter user should use.
+
+"""Tools to read a UTF-8 text document from a URL which
+will be split into component words for printing.
+
+Script usage:
+    python words.py <URL>
+"""
+
+import sys
+
 from urllib.request import urlopen
 
-def fetch_words():
-    with urlopen('http://sixty-north.com/c/t.txt') as story:
+def fetch_words(url):
+    """Fetch a list of words from a URL.
+
+    Args:
+        url: The URL of a UTF-8 text document.
+
+    Returns:
+        A list of strings containing the words from
+        the document.
+    """
+    with urlopen(url) as story:
         story_words = []
         for line in story:
             line_words = line.decode('utf-8').split()
             for word in line_words:
                 story_words.append(word)
+    return story_words
 
-    for word in story_words:
-        print(word)
+def print_items(items):
+    """Print items one per line.
+
+    Args:
+        An iterable series of printable items.
+    """
+    for item in items:
+        print(item)
+
+def main(url):
+    """Print each word from a text document from a URL.
+
+    Args:
+        url: The URL of a UTF-8 text document.
+        """
+    words = fetch_words(url)
+    print_items(words)
 
 if __name__ == '__main__':
-    fetch_words()
+    main(sys.argv[1]) # The 0th arg is the module filename
 
